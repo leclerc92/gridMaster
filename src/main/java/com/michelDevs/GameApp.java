9c6b83c97c113101b59a75faef63c338ba302780
Leclerc Clement <clement.leclerc@rte-france.com>
2025-12-01 17:15:18 +0100

    modif

--- src/main/java/com/michelDevs/GameApp.java
+++ src/main/java/com/michelDevs/GameApp.java
@@ -1,4 +1,83 @@
 package com.michelDevs;
 
-public class GameApp {
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.Substation;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameApp extends Application {
+
+    private GameEngine engine = new GameEngine();
+    private Network network;
+    private Group root = new Group();
+    private Map<String, Point2> nodeCoordonates = new HashMap<>();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        network = engine.getNetwork();
+
+        int x = 200;
+        for (Substation substation : network.getSubstations()) {
+            nodeCoordonates.put(substation.getId(), new Point(x, 300));
+            x += 400;
+        }
+
+        drawNetwork();
+
+        Scene scene = new Scene(root, 800, 600, Color.BLACK); // Fond noir style "Scada"
+        primaryStage.setTitle("PowSyBl Grid Master");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void drawNetwork() {
+        // --- TON DÉFI COMMENCE ICI ---
+
+        // Indice : Tu dois placer les éléments manuellement pour l'instant.
+        // Poste 1 à gauche (x=200, y=300)
+        // Poste 2 à droite (x=600, y=300)
+
+        // 1. Dessine un Cercle pour chaque Substation du réseau
+        // Pour l'instant, fais-le "en dur" ou essaie de boucler sur network.getSubstations()
+        // et de leur attribuer des coordonnées arbitraires.
+
+        // 2. Dessine une Ligne JavaFX pour relier les deux.
+
+        // Exemple pour t'aider à démarrer :
+        /*
+        Circle node1 = new Circle(200, 300, 20, Color.CYAN);
+        root.getChildren().add(node1);
+        */
+
+        for (Substation substation : network.getSubstations()) {
+            Circle node1 = new Circle(
+                    nodeCoordonates.get(substation.getId()).x,
+                    nodeCoordonates.get(substation.getId()).y,
+                    20,
+                    Color.CYAN);
+            node1.setId(substation.getId());
+            root.getChildren().add(node1);
+        }
+
+        Line line = new Line(200, 300, 600, 300);
+        line.setStroke(Color.WHITE);
+        line.setId("Ligne_HT");
+        root.getChildren().add(line);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/com/michelDevs/GameEngine.java
+++ src/main/java/com/michelDevs/GameEngine.java
@@ -1,4 +1,71 @@
 package com.michelDevs;
 
+import com.powsybl.iidm.network.*;
+
 public class GameEngine {
+
+
+    public Network getNetwork() {
+
+        Network network = Network.create("Def","MyGameNetwork");
+
+        //PRODUCTION
+
+        Substation s1 = network.newSubstation()
+                .setId("Poste_soure")
+                .setCountry(Country.FR)
+                .add();
+
+        VoltageLevel vl1 = s1.newVoltageLevel()
+                .setId("VL_source_400")
+                .setNominalV(400.0)
+                .setTopologyKind(TopologyKind.BUS_BREAKER)
+                .add();
+
+        vl1.getBusBreakerView().newBus().setId("Bus1").add();
+
+        vl1.newGenerator()
+                .setId("Gen_Nucleaire")
+                .setMinP(0.0)
+                .setMaxP(1000.0)
+                .setVoltageRegulatorOn(true)
+                .setTargetP(100.0)
+                .setTargetV(400.0)
+                .setBus("Bus1")
+                .add();
+
+
+        //CONSOMMATION
+
+        Substation s2 = network.newSubstation()
+                .setId("Poste_Ville")
+                .setCountry(Country.FR)
+                .add();
+
+        VoltageLevel vl2 = s2.newVoltageLevel()
+                .setId("VL_Ville_400")
+                .setNominalV(400.0)
+                .setTopologyKind(TopologyKind.BUS_BREAKER)
+                .add();
+
+        vl2.getBusBreakerView().newBus().setId("Bus2").add();
+        vl2.newLoad()
+                .setId("Conso_paris")
+                .setP0(100.0)
+                .setQ0(0.0)
+                .setBus("Bus2")
+                .add();
+
+
+        //LIGNES
+
+        network.newLine()
+                .setId("Ligne_HT")
+                .setR(1.0).setX(10.0).setG1(0.0).setG2(0.0).setB1(0.0).setB2(0.0)
+                .setVoltageLevel1(vl1.getId()).setBus1("Bus1")
+                .setVoltageLevel2(vl2.getId()).setBus2("Bus2")
+                .add();
+
+        return network;
+    }
 }
